2f66ec4ac2c41834f6af3dd81a7d712c4a8fc3f3
Enos Andrade <enosads@gmail.com>
2021-06-30 19:04:01 -0300

    finished

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,46 @@
+import {Flex, Icon, IconButton, useBreakpointValue} from "@chakra-ui/react";
+import {Profile} from "./Profile";
+import {NotificationsNav} from "./NotificationsNav";
+import {SearchBox} from "./SearchBox";
+import {Logo} from "./Logo";
+import {useSidebarDrawer} from "../../contexts/SidebarDrawerContext";
+import {RiMenuLine} from "react-icons/ri";
+
+export function Header() {
+    const {onOpen} = useSidebarDrawer();
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true
+    })
+    return (
+        <Flex
+            as='header'
+            width='100%'
+            maxWidth={1480}
+            height='20'
+            mx='auto'
+            mt='4'
+            align='center'
+            px='6'
+        >
+            {!isWideVersion &&
+            <IconButton
+                aria-label='Open navigation'
+                icon={<Icon as={RiMenuLine}/>}
+                fontSize='24'
+                variant='unstyled'
+                onClick={onOpen}
+                mr='2'
+            />
+            }
+            <Logo/>
+            {isWideVersion && <SearchBox/>}
+
+            <Flex align='center' ml='auto'>
+                <NotificationsNav/>
+                <Profile showProfileData={isWideVersion}/>
+            </Flex>
+        </Flex>
+
+    );
+}
